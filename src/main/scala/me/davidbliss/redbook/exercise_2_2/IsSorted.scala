2171093797243ab0848a2e995f4c60ecb60a91bc
David <sourcecode@mailbot.com>
2019-02-09 20:09:59 +0000

    Exercise 2.2
    Implement isSorted

---
+++ src/main/scala/me/davidbliss/redbook/exercise_2_2/IsSorted.scala
@@ -0,0 +1,16 @@
+package me.davidbliss.redbook.exercise_2_2
+
+object IsSorted {
+
+  // exercise 2.2
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(i: Int): Boolean = {
+      if (i == as.length) true
+      else if (ordered(as(i - 1), as(i))) loop(i + 1)
+      else false
+    }
+
+    if (as.length == 1) true
+    else loop(1)
+  }
+}
---
+++ src/test/scala/me/davidbliss/redbook/exercise_2_2/IsSortedTest.scala
@@ -0,0 +1,53 @@
+package me.davidbliss.redbook.exercise_2_2
+
+import org.scalatest.{FunSuite, Matchers}
+import IsSorted.isSorted
+
+class IsSortedTest extends FunSuite with Matchers {
+
+  def lt(a: Int, b: Int): Boolean = a < b
+
+  def gt(a: Int, b: Int): Boolean = a > b
+
+  test("Unsorted is not sorted") {
+    val values = Array(3, 2, 1, 3, 5)
+    isSorted(values, lt) shouldBe false
+    isSorted(values, gt) shouldBe false
+  }
+
+  test("Sorted in ascending order") {
+    val values = Array(3, 4, 5, 6, 7, 8, 9)
+    isSorted(values, lt) shouldBe true
+    isSorted(values, gt) shouldBe false
+  }
+
+  test("Sorted in ascending order except last") {
+    val values = Array(3, 4, 5, 6, 7, 8, 9, 0)
+    isSorted(values, lt) shouldBe false
+    isSorted(values, gt) shouldBe false
+  }
+
+  test("Sorted in ascending order with gaps") {
+    val values = Array(2, 4, 6, 8, 10)
+    isSorted(values, lt) shouldBe true
+    isSorted(values, gt) shouldBe false
+  }
+
+  test("Sorted in descending order") {
+    val values = Array(9, 8, 7, 6, 5, 4, 3, 2, 1)
+    isSorted(values, lt) shouldBe false
+    isSorted(values, gt) shouldBe true
+  }
+
+  test("Sorted in descending order except last") {
+    val values = Array(9, 8, 7, 6, 5, 4, 3, 2, 1, 10)
+    isSorted(values, lt) shouldBe false
+    isSorted(values, gt) shouldBe false
+  }
+
+  test("Sorted in descending order with gaps") {
+    val values = Array(10, 8, 6, 4, 2)
+    isSorted(values, lt) shouldBe false
+    isSorted(values, gt) shouldBe true
+  }
+}
